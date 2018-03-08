3593b6e3e4f391a97a3ca362ebbd9decfe320a89
Michael Tsai <hstsai@microsoft.com>
2018-03-08 17:24:18 +0800

    Upgrade .NET Framework to 4.6.1 for PTF

--- src/build.cmd
+++ src/build.cmd
@@ -7,31 +7,37 @@ if not defined buildtool (
 	for /f %%i in ('dir /b /ad /on "%windir%\Microsoft.NET\Framework\v4*"') do (@if exist "%windir%\Microsoft.NET\Framework\%%i\msbuild".exe set buildtool=%windir%\Microsoft.NET\Framework\%%i\msbuild.exe)
 )
 
+SET MSBUILDEXTPATH32 = "%programfiles(x86)%\MSBuild\"
+
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+	set buildtool="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
+)
+
 if not defined buildtool (
-	echo No msbuild.exe was found, install .Net Framework version 4.0 or higher
+	echo No msbuild.exe was found, install .Net Framework version 4.6 or higher
 	goto :eof
 )
 
 if not defined WIX (
-	echo WiX Toolset version 3.10 should be installed
+	echo WiX Toolset version 3.11 should be installed
 	goto :eof
 )
 
-if "%WIX:3.10=%"=="%WIX%" (
-	echo WiX Toolset version 3.10 should be installed
+if "%WIX:3.11=%"=="%WIX%" (
+	echo WiX Toolset version 3.11 should be installed
 	goto :eof
 )
 
 :: Check if visual studio or test agent is installed, since HtmlTestLogger depends on that.
 if not defined vspath (
-	if defined VS110COMNTOOLS (
-		set vspath="%VS110COMNTOOLS%"
-	) else if defined VS120COMNTOOLS (
-		set vspath="%VS120COMNTOOLS%"
+	if defined VS150COMNTOOLS (
+		set vspath="%VS150COMNTOOLS%"
 	) else if defined VS140COMNTOOLS (
 		set vspath="%VS140COMNTOOLS%"
+	) else if defined VS120COMNTOOLS (
+		set vspath="%VS120COMNTOOLS%"
 	) else (
-		echo Visual Studio or Visual Studio test agent should be installed, version 2012 or higher
+		echo Visual Studio or Visual Studio test agent should be installed, version 2013 or higher
 		goto :eof
 	)
 )
