1e616e4c6e6d940c00d5013dda1f689519cabc90
Michael Tsai <hstsai@microsoft.com>
2018-03-09 11:37:16 +0800

    Update .NET Framework version to 4.6.1

--- src/build.cmd
+++ src/build.cmd
@@ -7,12 +7,13 @@ if not defined buildtool (
 	for /f %%i in ('dir /b /ad /on "%windir%\Microsoft.NET\Framework\v4*"') do (@if exist "%windir%\Microsoft.NET\Framework\%%i\msbuild".exe set buildtool=%windir%\Microsoft.NET\Framework\%%i\msbuild.exe)
 )
 
+:: Use Visual Studio 2017 if existed
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
 	set buildtool="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
 )
 
 if not defined buildtool (
-	echo No msbuild.exe was found, install .Net Framework version 4.6 or higher
+	echo No msbuild.exe was found, install .Net Framework version 4.6.1 or higher
 	goto :eof
 )
 
