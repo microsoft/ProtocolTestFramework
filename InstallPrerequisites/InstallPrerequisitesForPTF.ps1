3172ba086eec54a79aec5e0b19f4942cbf2e7c09
Michael Tsai <hstsai@microsoft.com>
2018-09-26 14:10:56 +0800

    Remove temporary folder and refine implementation

--- InstallPrerequisites/InstallPrerequisitesForPTF.ps1
+++ InstallPrerequisites/InstallPrerequisitesForPTF.ps1
@@ -1,50 +1,35 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-# Create a tempoary folder under current folder, which is used to store downloaded files.
-Function CreateTemporaryFolder
-{
-    #create temporary folder for downloading tools
-    $tempPath = (get-location).ToString() + "\" + [system.guid]::newguid().ToString()
-    Write-Host "Create temporary folder for downloading files"``
-    $outFile = New-Item -ItemType Directory -Path $tempPath
-    Write-Host "Temporary folder $outFile is created"
-
-    return $outFile.FullName
-}
-
 # Download files
-Function DownloadFiles
+Function DownloadFile
 {
     param(        
-        $Files
+        $File
     )
 
-	ForEach ($File in $Files)
+    $Uri = $prefix + $File.title
+    $OutFile = $File.title
+    try {
+        Invoke-WebRequest -Uri $Uri -OutFile $OutFile
+    }
+    catch
     {
-        $Uri = $prefix + $File.title
-        $OutFile = $tempFolder + "\" + $File.title
-        try {
-            Invoke-WebRequest -Uri $Uri -OutFile $OutFile
+        try
+        {
+            (New-Object System.Net.WebClient).DownloadFile($Uri, $OutFile)
         }
         catch
         {
-            try
-            {
-                (New-Object System.Net.WebClient).DownloadFile($Uri, $OutFile)
-            }
-            catch
-            {
-                Write-host "Download $item.Name failed with exception: $_.Exception.Message"
-                Return
-            }
+            Write-host "Download $item.Name failed with exception: $_.Exception.Message"
+            Return
         }
-    }    
+    }
 }
 
 $ifInstallPrerequisitesExist = Test-Path .\InstallPrerequisites.ps1
-$ifInstallVs2017Community = Test-Path .\InstallVs2017Community.cmd
-$ifPrerequisitesConfigExist = Test-Path .\PrerequisitesConfig.xml
+$ifInstallVs2017Community    = Test-Path .\InstallVs2017Community.cmd
+$ifPrerequisitesConfigExist  = Test-Path .\PrerequisitesConfig.xml
 if (!$ifInstallPrerequisitesExist -or
     !$ifInstallVs2017Community -or
     !$ifPrerequisitesConfigExist) {
@@ -53,17 +38,23 @@ if (!$ifInstallPrerequisitesExist -or
         $prefix = 'https://raw.githubusercontent.com/Microsoft/WindowsProtocolTestSuites/staging/InstallPrerequisites/'
         $WebResponse = Invoke-WebRequest https://github.com/Microsoft/WindowsProtocolTestSuites/tree/staging/InstallPrerequisites/ -UseBasicParsing
 
-        # Download all .cmd Files
-        $CmdFiles = $WebResponse.Links |?{$_.href -match ".cmd"}
-        DownloadFiles -Files $CmdFiles
+        # Download InstallVs2017Community.cmd
+        if (!$ifInstallVs2017Community) {
+            $CmdFile = $WebResponse.Links |?{$_.href -match "InstallVs2017Community.cmd"}
+            DownloadFile -File $CmdFile
+        }
 
-        # Download all .xml Files
-        $XmlFiles = $WebResponse.Links |?{$_.href -match ".xml"}
-        DownloadFiles -Files $XmlFiles
+        # Download PrerequisitesConfig.xml
+        if (!$ifPrerequisitesConfigExist) {
+            $XmlFile = $WebResponse.Links |?{$_.href -match "PrerequisitesConfig.xml"}
+            DownloadFile -File $XmlFile
+        }
 
-        # Download all .ps1 Files
-        $Ps1Files = $WebResponse.Links |?{$_.href -match ".ps1"}
-        DownloadFiles -Files $Ps1Files
+        # Download InstallPrerequisites.ps1
+        if (!$ifInstallPrerequisitesExist) {
+            $Ps1File = $WebResponse.Links |?{$_.href -match "InstallPrerequisites.ps1"}
+            DownloadFile -File $Ps1File
+        }
     }
     catch {
         Write-Host "Please download the following InstallPrerequisites-related files manually from Github page https://github.com/Microsoft/WindowsProtocolTestSuites/tree/staging/InstallPrerequisites"
