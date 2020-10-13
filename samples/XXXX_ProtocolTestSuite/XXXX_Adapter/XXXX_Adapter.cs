c680f159a1aa7f568f94b8040f7b399956b7d5fd
Victor Ding <shoding@microsoft.com>
2020-10-13 07:12:01 +0000

    Merged PR 5274531: [28598278]: Update sample project in PTF repo
    Upgrade to .NETCore 3.1 and use nuget PTF
    
    Related work items: #28598278

--- samples/XXXX_ProtocolTestSuite/XXXX_Adapter/XXXX_Adapter.cs
+++ samples/XXXX_ProtocolTestSuite/XXXX_Adapter/XXXX_Adapter.cs
@@ -1,12 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved. 
 // Licensed under the MIT license. See LICENSE file in the project root for full license information. 
 
-using System;
 using Microsoft.Protocols.TestTools;
-using Microsoft.Protocols.TestTools.ExtendedLogging;
 
 namespace Microsoft.Protocols.TestSuites.XXXX.Adapter
-{   
+{
     /// <summary>
     /// Implements the XXXX protocol adapter
     /// </summary>
@@ -27,11 +25,7 @@ namespace Microsoft.Protocols.TestSuites.XXXX.Adapter
         public bool SendRequest(string SUTIPAddress)
         {
             // Add code here to construct a requst message and then send it to SUT
-
-            // Dump request message to ETW provider: Protocol-Test-Suite
-            // Then the message data could be captured using an ETW capture tool.
             // It's useful when the message is encrypted.
-            ExtendedLogger.DumpMessage("XXXX: Request", System.Text.Encoding.Default.GetBytes("Here is the binary of the request"));
             return true;
         }
 
@@ -44,12 +38,10 @@ namespace Microsoft.Protocols.TestSuites.XXXX.Adapter
         public bool WaitForResponse(out int status, int timeout)
         {
             // Add code here to receive data from SUT, and parse the data to a message structure
+            // It's useful when the message is encrypted.
             status = 0;
 
-            // Dump response message to ETW provider: Protocol-Test-Suite
-            // Then the message data could be captured using an ETW capture tool.
             // It's useful when the message is encrypted.
-            ExtendedLogger.DumpMessage("XXXX: Response", System.Text.Encoding.Default.GetBytes("Here is the binary of the response"));
             return true;
         }
     }
